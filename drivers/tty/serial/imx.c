6e88dd36651cf7031b7dac797cfcc87a8684cd3f
Stefan Eichenberger <stefan.eichenberger@toradex.com>
2024-06-21 17:37:49 +0200

    serial: imx: set receiver level before starting uart
    commit a81dbd0463eca317eee44985a66aa6cc2ce5c101 upstream.
    
    Set the receiver level to something > 0 before calling imx_uart_start_rx
    in rs485_config. This is necessary to avoid an interrupt storm that
    might prevent the system from booting. This was seen on an i.MX7 device
    when the rs485-rts-active-low property was active in the device tree.
    
    Fixes: 6d215f83e5fc ("serial: imx: warn user when using unsupported configuration")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Stefan Eichenberger <stefan.eichenberger@toradex.com>
    Link: https://lore.kernel.org/r/20240621153829.183780-1-eichest@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -1972,8 +1972,10 @@ static int imx_uart_rs485_config(struct uart_port *port,
 
 	/* Make sure Rx is enabled in case Tx is active with Rx disabled */
 	if (!(rs485conf->flags & SER_RS485_ENABLED) ||
-	    rs485conf->flags & SER_RS485_RX_DURING_TX)
+	    rs485conf->flags & SER_RS485_RX_DURING_TX) {
+		imx_uart_setup_ufcr(sport, TXTL_DEFAULT, RXTL_DEFAULT);
 		imx_uart_start_rx(port);
+	}
 
 	port->rs485 = *rs485conf;
 
